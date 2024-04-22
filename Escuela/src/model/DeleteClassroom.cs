ccee55fffcaf2403a22195577b27ee94ff1328d7
lauta <altairdiaz245@gmail.com>
2024-04-22 19:46:01 -0300

    Funcionalidad;

--- Escuela/src/model/DeleteClassroom.cs
+++ Escuela/src/model/DeleteClassroom.cs
@@ -1,6 +1,28 @@
-namespace Model.DeleteClassrooms;
+using ConsoleApp.PostgreSQL;
+using Helper.Responses;
 
+namespace Model.DeleteClassrooms;
 class DeleteClassroom
 {
-  
+  private readonly SchoolCtx _db;
+
+  public DeleteClassroom(SchoolCtx db) {
+    _db = db;
+  }
+
+  public R Remove(string id)
+  {
+    var db = _db;
+    var checkUser = db.classroom.FirstOrDefault(x => x.Id == id);
+    
+    if (checkUser == null)
+    {
+      return new ResponseBuilder("La clase no existe", 404).GetResult(); 
+    }
+
+    db.classroom.Remove(checkUser);
+    db.SaveChanges();
+    return new ResponseBuilder("Clase eliminada con exito", 200).GetResult();
+  }
+
 }
