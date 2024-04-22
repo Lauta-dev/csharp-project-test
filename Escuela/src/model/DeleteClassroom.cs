28209d924e2963bbc14d67911ca03d7b1924d321
lauta <altairdiaz245@gmail.com>
2024-04-22 20:06:45 -0300

    Enves de devolver un texto, devuelve un JSON con los datos correspondiente

--- Escuela/src/model/DeleteClassroom.cs
+++ Escuela/src/model/DeleteClassroom.cs
@@ -1,28 +1,33 @@
 using ConsoleApp.PostgreSQL;
 using Helper.Responses;
+using Helper.HttpStatusCodes;
 
 namespace Model.DeleteClassrooms;
 class DeleteClassroom
 {
   private readonly SchoolCtx _db;
 
-  public DeleteClassroom(SchoolCtx db) {
+  public DeleteClassroom(SchoolCtx db)
+  {
     _db = db;
   }
-
+  
   public R Remove(string id)
   {
     var db = _db;
     var checkUser = db.classroom.FirstOrDefault(x => x.Id == id);
-    
+    string message;
+    int statusCode;
+
     if (checkUser == null)
     {
-      return new ResponseBuilder("La clase no existe", 404).GetResult(); 
+      message = "La clase no existe";
+      statusCode = Codes.NotFound;
+      return new ResponseBuilder(message, statusCode, new { message, statusCode }).GetResult(); 
     }
 
-    db.classroom.Remove(checkUser);
-    db.SaveChanges();
-    return new ResponseBuilder("Clase eliminada con exito", 200).GetResult();
+    message = "La clase fue eliminada";
+    statusCode = Codes.Ok;
+    return new ResponseBuilder(message, statusCode, new { message, statusCode }).GetResult(); 
   }
-
 }
