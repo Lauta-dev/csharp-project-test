628fd6f59682e08ca2933d68be86a6a51b8c843c
lauta <altairdiaz245@gmail.com>
2024-04-25 09:40:22 -0300

    Actualizado para crear un build de la app

--- Escuela/init_app.sh
+++ Escuela/init_app.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
-function ayuda() {
+watch_large="--watch"
+watch_less="-w"
+
+run_large="--run"
+run_less="-r"
+
+build_large="--build"
+build_less="-b"
+
+dotnet_build () {
+  dotnet build
+}
+
+dotnet_watch () {
+  dotnet watch run
+}
+
+dotnet_run () {
+  dotnet run
+}
+
+function show_help() {
   echo "
-  --watch
+  $watch_large | $watch_less
     Cada que cambie el archivo se vuelve a compilar la APP 
     Comando: dotnet watch run
 
-  --run
+  $run_large | $run_less
     Se ejecua una vez la aplicación
     Comando: dotnet run
+  
+  $build_large | $build_less
+    crea un build
   "
 }
 
 case "$1" in
-  "--help")
-    ayuda
+  "--help" | "-h")
+    show_help
   ;;
-  "-h")
-    ayuda
+  $watch_large | $watch_less)
+     dotnet_watch
   ;;
-  "--watch")
-    dotnet watch run
+  $run_large | $run_less)
+    dotnet_run
   ;;
-  "--run")
-    dotnet run
+  $build_large | $build_less)
+    dotnet_build
   ;;
   *)
-    ayuda
+    show_help
   ;;
 esac
