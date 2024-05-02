45846481ab057cedb721049d58b202468d09db68
lauta <altairdiaz245@gmail.com>
2024-05-02 20:33:51 -0300

    format: formateo

--- Escuela/src/model/PostStudents.cs
+++ Escuela/src/model/PostStudents.cs
@@ -3,91 +3,30 @@ using ConsoleApp.PostgreSQL;
 using Helper.Responses;
 using Npgsql;
 using Helper.HttpStatusCodes;
+
 namespace Model.PostStudents;
 class PostStudents
 {
 
   private readonly SchoolCtx _db;
 
-  public PostStudents(SchoolCtx db)
-  {
-    _db = db;
-  }
+  public PostStudents(SchoolCtx db) { _db = db; }
 
-  public ResponseModel S(Student[] alumnos)
+  public ResponseModel S(Student[] students)
   {
+    foreach (Student student in students)
+    {
+      var aula = _db.classroom.FirstOrDefault(e => e.Id == student.ClassroomsId);
+      if (aula is null) return new ResponseBuilder("No existe el aula", 404).GetResult();
+      if (CheckStudent(student).httpCode != Codes.Ok) return CheckStudent(student);
+    }
+    
     try
     {
-      string message = "";
-      foreach (Student a in alumnos)
-      {
-        var aula = _db.classroom.FirstOrDefault(e => e.Id == a.ClassroomsId);
-        if (aula is null) return new ResponseBuilder("No existe el aula", 404).GetResult();
-        if (a.Name == null)
-        {
-          message = "El valor name debe tener el nombre del estudiante";
-          return new ResponseBuilder(
-            message,
-            Codes.BadRequest,
-            new 
-            {
-              pass = false,
-              message,
-              statusCode = Codes.BadRequest
-            }
-          ).GetResult();
-        }
+      _db.student.AddRange(students);
+      _db.SaveChanges();
 
-        if (a.LastName == null)
-        {
-          message = "El valor name debe tener el nombre del estudiante";
-          return new ResponseBuilder(
-            message,
-            Codes.BadRequest,
-            new 
-            {
-              pass = false,
-              message,
-              statusCode = Codes.BadRequest
-            }
-          ).GetResult();
-        }
-
-        if (a.Age == null)
-        {
-          message = "El valor name debe tener el nombre del estudiante";
-            return new ResponseBuilder(
-            message,
-            Codes.BadRequest,
-            new 
-            {
-              pass = false,
-              message,
-              statusCode = Codes.BadRequest
-            }
-          ).GetResult();
-        }
-
-        if (a.Age > 13 && a.Age < 18)
-        {
-          message = "El valor name debe tener el nombre del estudiante";
-            return new ResponseBuilder(
-            message,
-            Codes.BadRequest,
-            new 
-            {
-              pass = false,
-              message,
-              statusCode = Codes.BadRequest
-            }
-          ).GetResult();
-        }
-      }
-
-      _db.student.AddRange(alumnos);
-      //_db.SaveChanges();
-
-      return new ResponseBuilder("Alumnos añadidos", 200, alumnos).GetResult();
+      return new ResponseBuilder("Alumnos añadidos", 200, students).GetResult();
     }
     catch (System.Text.Json.JsonException e)
     {
@@ -114,5 +53,72 @@ class PostStudents
       return new ResponseBuilder("Error por nose server", 500).GetResult();
 
     }
+  }
+
+  private ResponseModel CheckStudent (Student student)
+  {
+    string message = "";
+
+    if (student.Name == null)
+    {
+      message = "El valor name debe tener el nombre del estudiante";
+      return new ResponseBuilder(
+        message,
+        Codes.BadRequest,
+        new 
+        {
+          pass = false,
+          message,
+          statusCode = Codes.BadRequest
+        }
+      ).GetResult();
+    }
+
+    if (student.LastName == null)
+    {
+      message = "El valor name debe tener el nombre del estudiante";
+      return new ResponseBuilder(
+        message,
+        Codes.BadRequest,
+        new 
+        {
+          pass = false,
+          message,
+          statusCode = Codes.BadRequest
+        }
+      ).GetResult();
+    }
+
+    if (student.Age == 0)
+    {
+      message = "El valor name debe tener el nombre del estudiante";
+        return new ResponseBuilder(
+        message,
+        Codes.BadRequest,
+        new 
+        {
+          pass = false,
+          message,
+          statusCode = Codes.BadRequest
+        }
+      ).GetResult();
+    }
+
+    if (student.Age > 13 && student.Age < 18)
+    {
+      message = "El valor name debe tener el nombre del estudiante";
+        return new ResponseBuilder(
+        message,
+        Codes.BadRequest,
+        new 
+        {
+          pass = false,
+          message,
+          statusCode = Codes.BadRequest
+        }
+      ).GetResult();
+    }
+  
+    return new ResponseBuilder("Ok", Codes.Ok).GetResult();
   }
 }
