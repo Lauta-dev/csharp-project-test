a38f5c2bb4282eedf348bf8afc1c5b5f24f07233
lauta <altairdiaz245@gmail.com>
2024-05-02 22:13:38 -0300

    add: message for studen

--- Escuela/src/model/Const/Message.cs
+++ Escuela/src/model/Const/Message.cs
@@ -2,6 +2,16 @@ namespace Model.Const.Message;
 public static class Messages
 {
   // Classrooms
-  public const string ClassroomsNotFounds = "No hay alumnos";
-  public const string ClassroomsFounds = "Paso con exito";  
+  public const string ClassroomsNotFounds = "The classroom does not exist";
+
+  // Post student
+  // --
+  public const string StudentNameIsEmply = "Name is null";
+  public const string StudentLastNameIsEmply = "Last name is null";
+  public const string StudentAgeIsEmply = "Age is emp";
+  public const string StudentAgeRangeErrorMessage = "Age invalidate. Age must be between 3 and 2";
+  public const string ErrorParceJson = "Error in parsing JSON";
+
+  // --
+  public const string SuccessfullyAdded = "Successfully added";
 }
