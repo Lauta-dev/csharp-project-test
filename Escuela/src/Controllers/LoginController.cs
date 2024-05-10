d4338d6ecae96055c5d73fecc2dae3e1adaaf49e
lauta <altairdiaz245@gmail.com>
2024-05-10 19:59:27 -0300

    add: jwt support

--- Escuela/src/Controllers/LoginController.cs
+++ Escuela/src/Controllers/LoginController.cs
@@ -1,12 +1,32 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using UserModel.Person;
+using src.GenerateJwts;
 using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Login.Controllers;
 public class Account : Controller
 {
-    public async Task<Object> Login(string returnUrl = "/")
+    private readonly IConfiguration _config;
+    public Account(IConfiguration config) { _config = config; }
+
+    [HttpPost]    
+    public Object Login([FromBody] PersonModel user)
     {
-        return "Login File";
+        string token = new GenerateJwt(_config).GenerateJSONWebToken(user);
+
+        System.Console.WriteLine("---------------------------------------------");
+        System.Console.WriteLine(user.id);
+        return token;
+    }
+
+    [Authorize]
+    public Object Auth()
+    {
+      string getToken = HttpContext.Request.Headers["Authorization"].ToString().Split("Bearer")[1].Trim();
+      var handler = new JwtSecurityTokenHandler().ReadJwtToken(getToken);
+      var payload = handler.Payload["nickname"];
+
+      return payload;
     }
 }
