0c0decfad209640b2150ca98eab9446636222917
lauta <altairdiaz245@gmail.com>
2024-05-13 10:34:20 -0300

    add: jwt validations

--- Escuela/src/Controllers/LoginController.cs
+++ Escuela/src/Controllers/LoginController.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using UserModel.Person;
 using src.GenerateJwts;
 using Microsoft.AspNetCore.Authorization;
 using System.IdentityModel.Tokens.Jwt;
+using Helper.BasicAuthInfo;
+using Helper.ValidateEmails;
 
 namespace Login.Controllers;
 public class Account : Controller
@@ -10,23 +11,52 @@ public class Account : Controller
     private readonly IConfiguration _config;
     public Account(IConfiguration config) { _config = config; }
 
-    [HttpPost]    
-    public Object Login([FromBody] PersonModel user)
+    [HttpPost]
+    public Object Login([FromBody] Info user)
     {
-        string token = new GenerateJwt(_config).GenerateJSONWebToken(user);
+        // TODO:
+        // - Validar que el mail exista en la base de datos
+        // - Dependiento si es alumno o profesor se le da un rol
 
-        System.Console.WriteLine("---------------------------------------------");
-        System.Console.WriteLine(user.id);
-        return token;
+        string res = "Mail no valido";
+
+        if (Validate.Mail(user.mail))
+            return Results.Ok(new { access_token = new GenerateJwt(_config).GenerateJSONWebToken(user) });
+
+
+        return Results.BadRequest(res);
     }
 
     [Authorize]
     public Object Auth()
     {
-      string getToken = HttpContext.Request.Headers["Authorization"].ToString().Split("Bearer")[1].Trim();
-      var handler = new JwtSecurityTokenHandler().ReadJwtToken(getToken);
-      var payload = handler.Payload["nickname"];
+        string getToken = HttpContext.Request.Headers["Authorization"].ToString().Split("Bearer")[1].Trim();
 
-      return payload;
+        try
+        {
+            var handler = new JwtSecurityTokenHandler().ReadJwtToken(getToken);
+            var payload = handler.Payload["mail"].ToString();
+
+            if (String.IsNullOrEmpty(payload))
+                throw new ArgumentNullException();
+
+            return payload;
+        }
+        catch (ArgumentNullException ex)
+        {
+            return StatusCode(404, ex.Message);
+        }
+        catch (Microsoft.IdentityModel.Tokens.SecurityTokenMalformedException ex)
+        {
+            return StatusCode(404, ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+            return StatusCode(404, ex.Message);
+        }
+        catch (System.Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 }
