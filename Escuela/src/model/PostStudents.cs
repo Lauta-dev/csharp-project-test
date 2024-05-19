d5824769b5b8bd3df1045253196b26abbf9c7b2c
lauta <altairdiaz245@gmail.com>
2024-05-18 21:00:55 -0300

    feat: save student in db wirh hash password

--- Escuela/src/model/PostStudents.cs
+++ Escuela/src/model/PostStudents.cs
@@ -1,7 +1,13 @@
+using System.Security.Cryptography;
+using System.Text;
 using ConsoleApp.PostgreSQL;
+using dto.MappStudentDto;
+using dto.StudentDto;
 using Escuela.Models.Alumno;
+using Helper.HashText;
 using Helper.HttpStatusCodes;
 using Helper.Responses;
+using Helper.UUID;
 using Model.Const.Age;
 using Model.Const.Message;
 
@@ -16,59 +22,71 @@ class PostStudents
     _db = db;
   }
 
-  public ResponseModel AddStudents(Student[] students)
+  public ResponseModel AddStudents(StudentDto[] students)
   {
-    foreach (Student student in students)
+    string[] uuid;
+    string sal;
+    byte[] passwordHashed;
+
+    List<Student> studentList = new List<Student>();
+
+    foreach (StudentDto student in students)
     {
-      var aula = _db.classroom.FirstOrDefault(e => e.Id == student.ClassroomsId);
+      var aula = _db.classroom.FirstOrDefault(e => e.Id == student.classrooms_id);
       if (aula == null)
         return new ResponseBuilder(Messages.ClassroomsNotFounds, Codes.BadRequest).GetResult();
+
       if (CheckStudent(student).httpCode != Codes.Ok)
         return CheckStudent(student);
+
+      uuid = UUID.GenerateUUID().Split("-");
+      sal = uuid[0] + uuid[4];
+      passwordHashed = Hashing.Hash(sal + student.password);
+
+      studentList.Add(Mapper.MapToStudent(student, sal, passwordHashed));
     }
 
     try
     {
-      _db.student.AddRange(students);
+      _db.student.AddRange(studentList);
       _db.SaveChanges();
-
-      return Fu(Messages.SuccessfullyAdded, Codes.Ok);
+      _db.Dispose();
+      return Response(Messages.SuccessfullyAdded, Codes.Ok);
     }
     catch (System.Text.Json.JsonException e)
     {
-      return Fu(e.Message);
+      return Response(e.Message);
     }
     catch (System.Exception e)
     {
-      return Fu(e.Message, Codes.InternalServerError);
+      return Response(e.Message, Codes.InternalServerError);
     }
   }
 
-  private ResponseModel Fu(string message = "not message", int statusCode = Codes.BadRequest)
+  private ResponseModel Response(string message = "not message", int statusCode = Codes.BadRequest)
   {
     return new ResponseBuilder(message, statusCode, new { message, statusCode }).GetResult();
   }
 
-  private ResponseModel CheckStudent(Student student)
+  private ResponseModel CheckStudent(StudentDto student)
   {
-    string name = student.Name;
-    string lastName = student.LastName;
-    int age = student.Age;
-    int rol = student.Rol;
-    string mail = student.Mail;
+    string name = student.name;
+    string lastName = student.last_name;
+    int age = int.Parse(student.age);
+    string mail = student.mail;
 
     if (string.IsNullOrEmpty(name))
-      return Fu(Messages.StudentNameIsEmply);
+      return Response(Messages.StudentNameIsEmply);
 
     if (string.IsNullOrEmpty(lastName))
-      return Fu(Messages.StudentLastNameIsEmply);
+      return Response(Messages.StudentLastNameIsEmply);
 
     if (age == DefaultAge.Zero)
-      return Fu(Messages.StudentAgeIsEmply);
+      return Response(Messages.StudentAgeIsEmply);
 
     if (age > DefaultAge.MinAge && age < DefaultAge.MaxAge)
-      return Fu(Messages.StudentAgeRangeErrorMessage);
+      return Response(Messages.StudentAgeRangeErrorMessage);
 
-    return Fu("Ok", Codes.Ok);
+    return Response("Ok", Codes.Ok);
   }
 }
