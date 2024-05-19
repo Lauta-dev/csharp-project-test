535055bdb9c82745a7ef2064a3f4363c2ac75899
lauta <altairdiaz245@gmail.com>
2024-05-18 21:02:56 -0300

    use: dto

--- Escuela/src/di/SchoolManagement.cs
+++ Escuela/src/di/SchoolManagement.cs
@@ -1,4 +1,4 @@
-using Escuela.Models.Alumno;
+using dto.StudentDto;
 using Helper.Responses;
 
 namespace SchoolManagement.AlumnoRe;
@@ -8,6 +8,6 @@ public interface IRequestAlumno
   public List<object> GetStudents();
   public object GetStudentById(string id);
   public object GetStudentByClassroom(string id, int limit);
-  public ResponseModel AddNewStudent(Student[] alumnos);
+  public ResponseModel AddNewStudent(StudentDto[] alumnos);
   public Task<ResponseModel> RemoveStudent(string id);
 }
