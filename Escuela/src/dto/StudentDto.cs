44bc96338a119b4c4020a1f3f6b83ef034c3209a
lauta <altairdiaz245@gmail.com>
2024-05-30 20:32:24 -0300

    refactor: now inherits from BasePersonDto

--- Escuela/src/dto/StudentDto.cs
+++ Escuela/src/dto/StudentDto.cs
@@ -1,12 +1,8 @@
+using dto.PersonDto;
+
 namespace dto.StudentDto;
 
-public class StudentDto
+public class StudentDto : BasePersonDto
 {
-  public string name { get; set; }
-  public string last_name { get; set; }
-  public string age { get; set; }
-  public DateTime date_of_birth { get; set; }
   public string classrooms_id { get; set; }
-  public string mail { get; set; }
-  public string password { get; set; }
 }
