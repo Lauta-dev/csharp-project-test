71b024d1ac16d2038c59adfa77078947b5dee225
lauta <altairdiaz245@gmail.com>
2024-05-30 20:27:37 -0300

    refactor: support for dependency injection

--- Escuela/src/di/CheckUserServices.cs
+++ Escuela/src/di/CheckUserServices.cs
@@ -8,10 +8,17 @@ namespace CheckUserManagent;
 
 public class CheckUserServices : ICheckUser
 {
-  private readonly SchoolCtx _db = new SchoolCtx();
+  private readonly SchoolCtx _db;
+  private readonly IConfiguration _config;
+
+  public CheckUserServices(SchoolCtx db, IConfiguration config)
+  {
+    _db = db;
+    _config = config;
+  }
 
   public ResponseModel CheckUser(Info info)
   {
-    return new CheckUsers(_db).Exist(info);
+    return new CheckUsers(_db, _config).Exist(info);
   }
 }
