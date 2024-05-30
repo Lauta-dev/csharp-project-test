98d0a22e3c2a3a1a0fad25b9edbe469760ea6841
lauta <altairdiaz245@gmail.com>
2024-05-30 20:37:13 -0300

    feat: authtenticacion user

--- Escuela/src/model/CheckIfExistUser.cs
+++ Escuela/src/model/CheckIfExistUser.cs
@@ -1,27 +1,64 @@
 using ConsoleApp.PostgreSQL;
+using Escuela.Models.Admins;
 using Escuela.Models.Alumno;
+using Escuela.Models.Base;
+using Escuela.Models.TeacherModel;
 using Helper.BasicAuthInfo;
 using Helper.HashText;
 using Helper.HttpStatusCodes;
 using Helper.Responses;
+using src.GenerateJwts;
 
 namespace Model.CheckIfExistUser;
+
+public class Persona
+{
+  public Student student;
+  public TeacherModel teacher;
+  public Admin admin;
+}
 
 class CheckUsers
 {
   private readonly SchoolCtx _db;
+  private readonly IConfiguration _config;
 
-  public CheckUsers(SchoolCtx db)
+  public CheckUsers(SchoolCtx db, IConfiguration config)
   {
     _db = db;
+    _config = config;
   }
 
   public ResponseModel Exist(Info info)
   {
-    Student? studentDb = _db.student.FirstOrDefault(student => student.Mail == info.mail);
+    string mail = info.mail;
+    string password = info.password;
     string message = "Okay";
+    int rol = 0;
+    bool pass = false;
 
-    if (studentDb == null)
+    Student? studentDb = _db.student.FirstOrDefault(student => student.Mail == mail);
+    TeacherModel? teacherDb = _db.teacher.FirstOrDefault(teacher => teacher.Mail == mail);
+    Admin? adminDb = _db.admin.FirstOrDefault(admin => admin.Mail == mail);
+
+    Person[] person = { studentDb, teacherDb, adminDb };
+
+    for (int i = 0; i < person.Length; i++)
+    {
+      if (person[i] != null)
+      {
+        password = person[i].Sal + password;
+
+        if (Hashing.Compare(password, person[i].Password))
+        {
+          rol = person[i].Rol;
+          pass = true;
+          break;
+        }
+      }
+    }
+
+    if (!pass)
     {
       message = "Unauthorized";
       return new ResponseBuilder(
@@ -31,23 +68,17 @@ class CheckUsers
       ).GetResult();
     }
 
-    string password = studentDb.Sal + info.password.Trim();
-    bool checkPass = Hashing.Compare(password, studentDb.Password);
-
-    if (!checkPass)
-    {
-      message = "Contraseña incorrecta";
-      return new ResponseBuilder(
-        message,
-        Codes.NotAcceptable,
-        new { statusCode = Codes.Unauthorized, message }
-      ).GetResult();
-    }
+    string token = new GenerateJwt(_config).GenerateJSONWebToken(info, rol);
 
     return new ResponseBuilder(
       message,
       Codes.Ok,
-      new { statusCode = Codes.Ok, message }
+      new
+      {
+        statusCode = Codes.Ok,
+        message,
+        accessToken = token
+      }
     ).GetResult();
   }
 }
