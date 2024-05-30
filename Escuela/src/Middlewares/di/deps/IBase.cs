b800268d50d8a3e68f6bb29ca04898f27de2880a
lauta <altairdiaz245@gmail.com>
2024-05-30 20:26:14 -0300

    feat: update interface

--- Escuela/src/Middlewares/di/deps/IBase.cs
+++ Escuela/src/Middlewares/di/deps/IBase.cs
@@ -5,4 +5,6 @@ interface IBase
   public string GetPath(HttpContext ctx);
   public string GetMethod(HttpContext ctx);
   public void SetStatusCode(HttpContext ctx, int code);
+  public IHeaderDictionary GetHeaders(HttpContext ctx);
+  public string GetBearerToken(HttpContext ctx);
 }
